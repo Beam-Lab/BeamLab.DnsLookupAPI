f28dc6f19ac816d1f592398517299337ee33938f
Emanuele B <bartolesiemanuele@hotmail.com>
2017-10-12 09:34:02 +0200

    first release

---
+++ BeamLab.DnsLookupAPI/Controllers/DnsController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using DnsClient;
+using DnsClient.Protocol;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BeamLab.DnsLookupAPI.Controllers
+{
+    [ApiVersion("1.0")]
+    [ApiVersion("2.0")]
+    [ApiVersion("3.0")]
+    //[Route("api/[controller]")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class DnsController : ControllerBase
+    {
+        private ILookupClient _client;
+
+        public DnsController(ILookupClient client)
+        {
+            _client = client;
+        }
+
+        /// <summary>
+        /// DNSs the query.
+        /// </summary>
+        /// <param name="domain">The domain.</param>
+        /// <returns></returns>
+        [MapToApiVersion("2.0")]
+        [MapToApiVersion("1.0")]
+        [HttpGet]
+        [Route("DnsQuery")]
+        public IActionResult DnsQuery(string domain)
+        {
+            var records = _client.Query(domain, QueryType.ANY).AllRecords.ToList();
+
+            return Ok(records);
+        }
+
+        /// <summary>
+        /// DNSs the query.
+        /// </summary>
+        /// <param name="domain">The domain.</param>
+        /// <param name="dns">The DNS server.</param>
+        /// <param name="dnsPort">The DNS server port.</param>
+        /// <returns></returns>
+        [MapToApiVersion("3.0")]
+        [HttpGet]
+        [Route("DnsQuery")]
+        public IActionResult DnsQuery(string domain,string dns = "8.8.8.8", int dnsPort = 53)
+        {
+            var endpoint = new IPEndPoint(IPAddress.Parse(dns), dnsPort);
+            var client = new LookupClient(endpoint)
+            {
+                UseCache = true
+
+            };
+
+            var records = _client.Query(domain, QueryType.ANY).AllRecords.ToList();
+
+            return Ok(records);
+        }
+
+        /// <summary>
+        /// Echoes the specified domain.
+        /// </summary>
+        /// <param name="domain">The domain.</param>
+        /// <returns></returns>
+        [MapToApiVersion("1.0")]
+        [HttpGet]
+        [Route("Echo")]
+        public string Echo(string domain)
+        {
+            return domain;
+        }
+    }
+}
---
+++ BeamLab.DnsLookupAPI/Startup.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.PlatformAbstractions;
+using Swashbuckle.AspNetCore.Swagger;
+using System.IO;
+using System.Net;
+using System.Reflection;
+using DnsClient;
+
+namespace BeamLab.DnsLookupAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var endpoint = new IPEndPoint(IPAddress.Parse("8.8.8.8"), 53);
+            var client = new LookupClient(endpoint)
+            {
+                UseCache = true
+            
+            };
+
+            services.AddSingleton<ILookupClient>(client);
+
+            services.AddMvcCore().AddVersionedApiExplorer(o => o.GroupNameFormat = "'v'VVV");
+
+            services.AddMvc();
+
+            services.AddApiVersioning(o => o.ReportApiVersions = true);
+
+            services.AddSwaggerGen(
+                options =>
+                {
+                    // resolve the IApiVersionDescriptionProvider service
+                    // note: that we have to build a temporary service provider here because one has not been created yet
+                    var provider = services.BuildServiceProvider().GetRequiredService<IApiVersionDescriptionProvider>();
+
+                    // add a swagger document for each discovered API version
+                    // note: you might choose to skip or document deprecated API versions differently
+                    foreach (var description in provider.ApiVersionDescriptions)
+                    {
+                        options.SwaggerDoc(description.GroupName, CreateInfoForApiVersion(description));
+                    }
+
+                    // add a custom operation filter which sets default values
+                    options.OperationFilter<SwaggerDefaultValues>();
+
+                    // integrate xml comments
+                    options.IncludeXmlComments(XmlCommentsFilePath);
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApiVersionDescriptionProvider provider)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(
+                options =>
+                {
+                    // build a swagger endpoint for each discovered API version
+                    foreach (var description in provider.ApiVersionDescriptions)
+                    {
+                        options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
+                    }
+                });
+
+            app.UseMvc();
+        }
+
+        static string XmlCommentsFilePath
+        {
+            get
+            {
+                var basePath = PlatformServices.Default.Application.ApplicationBasePath;
+                var fileName = typeof(Startup).GetTypeInfo().Assembly.GetName().Name + ".xml";
+                return Path.Combine(basePath, fileName);
+            }
+        }
+
+        static Info CreateInfoForApiVersion(ApiVersionDescription description)
+        {
+            var info = new Info()
+            {
+                Title = $"DNS API {description.ApiVersion}",
+                Version = description.ApiVersion.ToString(),
+                Description = "An application to query DNS.",
+                Contact = new Contact() { Name = "Emanuele Bartolesi", Email = "emanuele@beamlab.cloud" },
+                TermsOfService = "Free",
+                License = new License() { Name = "MIT", Url = "https://opensource.org/licenses/MIT" }
+            };
+
+            if (description.IsDeprecated)
+            {
+                info.Description += " This API version has been deprecated.";
+            }
+
+            return info;
+        }
+    }
+}
